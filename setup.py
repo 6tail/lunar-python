b73ef18892eeb0b1fc0864a94d06036cecd9c591
6tail <6tail@6tail.cn>
2020-09-23 16:00:32 +0800

    v1.0.15 修复安装时的编码兼容性问题

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
 from setuptools import setup
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.md')) as f:
-    long_description = f.read()
 
 setup(
     name='lunar_python',
-    version='1.0.14',
+    version='1.0.15',
     packages=['lunar_python', 'lunar_python.util', 'lunar_python.eightchar'],
     url='https://github.com/6tail/lunar-python',
     license='MIT',
     author='6tail',
     author_email='6tail@6tail.cn',
     description='lunar is a calendar library for Solar and Chinese Lunar.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description='lunar is a calendar library for Solar and Chinese Lunar.',
     keywords='solar lunar'
 )
